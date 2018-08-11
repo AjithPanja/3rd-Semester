d226be180f8c56356fc2d24237cd2a888091612f
AjithPanja <ajithkumarpanja@gmail.com>
2018-08-11 19:04:20 +0530

    Created Ex_6

--- Pds-2-Lab/Ex_6/ex_6_2.cpp
+++ Pds-2-Lab/Ex_6/ex_6_2.cpp
@@ -1,76 +1,44 @@
-/* Classes HouseEmi and CarEmi are derived from class Savings
-Class Total is derived from both classes HouseEmi and CarEmi
-Thus, making it Hybrid
-                      Savings
-                    ____|______
-                   |           |
-                HouseEmi     CarEmi
-                   |___________|
-                         |
-                       Total
-We are using Virtual keyword while inheriting Savings for
-HouseEmi and CarEmi..BEcause "setSavings" functions can be
-accessed in two ways..Total->House->Savings and Total->Car->Savings
-.if the virtual is not there, the compiler would throw an error
-"ambiguous" and ask you to show the path..if virtul keyword
-is there, it will choose its own path, automically.
-*/
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Savings
-{
-public:
-    int savings;
-    void setSavings(int x)
-    {
-        savings = x;
-    }
-};
-class HouseEmi : virtual public Savings
-{
-protected:
-    int h_emi;
-public:
-    void house_emi()
-    {
-        h_emi = savings / 10;
-    }
-};
-class CarEmi : virtual public Savings
-{
-protected:
-    int c_emi;
-public:
-    void car_emi()
-    {
-        c_emi = savings / 50;
-    }
-};
-class Total : public HouseEmi,public CarEmi
-{
-protected:
-    int total;
-public:
-    void display()
-    {
-    total = h_emi + c_emi;
-    savings = savings - total;
-    cout << "House Emi :" << h_emi << endl;
-    cout << "Car Emi :" << c_emi << endl;
-    cout << "Remaining Savings :" << savings << endl;
-    }
-};
-int main()
-{
-    Total t;
-    int x;
-    cout << "Enter the Savings :";
-    cin >> x;
-    t.setSavings(x);
-    t.house_emi();
-    t.setSavings(x);
-    t.car_emi();
-    t.display();
-    return 0;
-}
+class Shape {
+  protected:
+        int width, height;
+  public:
+    void setValues(int width, int height)
+      {
+          this->width=width;
+          this->height=height;
+      }
+      virtual int area()
+      {
 
+      }
+};
+
+class Rectangle: public Shape {
+  public:
+    int area()
+      {
+          return width*height;
+      }
+};
+
+class Triangle: public Shape {
+  public:
+    int area()
+      {
+          return width*height/2;
+      }
+};
+
+int main () {
+  Rectangle r;
+  Triangle t;
+  Shape *s1 = &r;
+  Shape *s2 = &t;
+  s1->setValues (4,5);
+  s2->setValues (5,10);
+  cout << s1->area() << endl;
+  cout << s2->area() << endl;
+  return 0;
+}
