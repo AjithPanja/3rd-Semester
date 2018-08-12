e9160cfbaf6e90a0f988a7780ea4b69cd88c53cd
AJITH KUMAR PANJA <ajithkumarpanja@gmail.com>
2018-08-12 17:41:17 +0530

    Update ex_6_1.cpp

--- Pds-2-Lab/Ex_6/ex_6_1.cpp
+++ Pds-2-Lab/Ex_6/ex_6_1.cpp
@@ -6,14 +6,28 @@ public:
     virtual void display()
     {
        cout << "Hi! This is Parent's Class.";
+    } 
+    void sum()
+    {
+        cout << "No Sum..Its main class.";
     }
 };
 class Child : public Parent
 {
+    int a,b;
 public:
+    Child(int x,int y)
+    {
+        a = x;
+        b = y;
+    }
     void display()
     {
         cout << "Hi! This is Child's Class.";
+    }
+    void sum()
+    {
+        cout << "Sum is :" << a+b;
     }
 };
 int main()
@@ -22,5 +36,6 @@ int main()
     Child c;
     p = &c;
     p->display(); //without the virtual keyword in the Parent class's display method, the ocompiler will get the Parent's display..this is called Early Binding
+    p->area();
     return 0;
 }
